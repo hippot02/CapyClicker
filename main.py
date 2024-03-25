49ef0a8b36b1a5182adaacc01cc9ead3f358abd3
Hippot <hippot02@gmail.com>
2024-03-25 12:59:05 +0100

    ajout sauvegarde de points

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 from PySide6.QtGui import QPixmap
 from PySide6.QtCore import Qt
 
+
 class ClickerWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.click_count = 0
+        self.click_count = self.load_click_count()
 
         self.initUI()
 
@@ -15,7 +16,7 @@ class ClickerWindow(QWidget):
         self.setWindowTitle('Clicker')
         self.setGeometry(100, 100, 300, 200)
 
-        self.click_label = QLabel('Nombre de Points: 0', self)
+        self.click_label = QLabel(f'Nombre de Points: {self.click_count}')
         self.click_label.setAlignment(Qt.AlignCenter)
 
         self.image_label = QLabel(self)
@@ -33,6 +34,19 @@ class ClickerWindow(QWidget):
     def on_click(self, event):
         self.click_count += 1
         self.click_label.setText(f'Nombre de Points: {self.click_count}')
+        self.save_click_count()
+
+    def load_click_count(self):
+        try:
+            with open('config.txt', 'r') as f:
+                return int(f.read())
+        except FileNotFoundError:
+            return 0
+
+    def save_click_count(self):
+        with open('config.txt', 'w') as f:
+            f.write(str(self.click_count))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
